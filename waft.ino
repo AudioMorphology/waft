5c090f974553c140be22fd419eaacb2f974258a3
AudioMorphology <richard.goodwin@morphology.co.uk>
2017-03-01 22:00:40 +0000

    minor update

--- waft.ino
+++ waft.ino
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+// 01-mar
 // See http://creativecommons.org/licenses/MIT/ for more information.
 
 #line 1 "waft.ino"
